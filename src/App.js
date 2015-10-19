6db32d004deaa2d6157164356abab840edabbf04
Danny Phillips <dannyp@procore.com>
2015-10-18 20:22:13 -0700

    removed old react-components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,5 @@
 import React, { Component } from 'react';
-import { NICE, SUPER_NICE } from './colors';
 import { RaisedButton, } from 'material-ui';
-
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
-}
 
 export class App extends Component {
   render() {
@@ -35,8 +8,6 @@ export class App extends Component {
         <RaisedButton label="Default" />
         <RaisedButton label="Primary" primary={true} />
         <RaisedButton label="Secondary" secondary={true} />
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
       </div>
     );
   }
