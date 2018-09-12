339817161fc7f5ca756c26e94f3b6bbf415499d1
Danny Phillips <dannyp@procore.com>
2018-09-12 15:54:52 -0700

    scaffold gatsby

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/dannyphillips/CodeProjects/dannyphillips.github.io/src/layouts/index.js"
+  import data from "/Users/dannyphillips/CodeProjects/dannyphillips.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
