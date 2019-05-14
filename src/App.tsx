5de364b019e6c8fbf6fb529102f5deed9d36c4aa
Danny Phillips <danny.phillips8@gmail.com>
2019-05-14 01:10:30 -0700

    setup app shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import AppShell from './AppShell'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <AppShell>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </AppShell>
     </div>
   );
 }
