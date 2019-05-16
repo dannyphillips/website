fad2e16119d5deab3a2e25bbcda2ccf5704f93d9
Danny Phillips <danny.phillips8@gmail.com>
2019-05-16 00:06:22 -0700

    converted to gatsby

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-![dp](https://user-images.githubusercontent.com/8366399/57826239-467d7d00-7757-11e9-9d95-0026ae578696.png)
-# dannyphillips
-
+# Danny Phillips
 All about me
---
+++ config/index.js
@@ -0,0 +1,25 @@
+module.exports = {
+  pathPrefix: "/", // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: "Danny Phillips", // Navigation and Site Title
+  siteTitleAlt: "Danny Phillips - All about me", // Alternative Site title for SEO
+  siteTitleManifest: "MinimalBlog",
+  siteUrl: "https://dannyphillips.me", // Domain of your site. No trailing slash!
+  siteLanguage: "en", // Language Tag on <html> element
+  siteHeadline: "All about me", // Headline for schema.org JSONLD
+  siteBanner: "/social/banner.jpg", // Your image for og:image tag. You can find it in the /static folder
+  favicon: "src/favicon.png", // Your image for favicons. You can find it in the /src folder
+  siteDescription:
+    "All about me. My resume, my blog and my projects.", // Your site description
+  author: "Danny Phillips", // Author for schemaORGJSONLD
+  siteLogo: "/social/logo.png", // Image for schemaORGJSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: "@_dannyjphillips", // Twitter Username - Optional
+  // googleAnalyticsID: "UA-47519312-6",
+
+  // Manifest and Progress color
+  // See: https://developers.google.com/web/fundamentals/web-app-manifest/
+  themeColor: "#3498DB",
+  backgroundColor: "#2b2e3c"
+};
