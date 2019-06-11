89fe388800f1556b9a48259f4e7d675c2cfac7ef
Danny Phillips <danny.phillips8@gmail.com>
2019-06-10 22:31:26 -0700

    add comments to gatsby-node

--- src/components/Blog/BlogPostTile.js
+++ src/components/Blog/BlogPostTile.js
@@ -47,7 +47,7 @@ const Excerpt = styled.p`
 
 const BlogPostTile = ({ title, date, excerpt, slug, timeToRead, tags }) => {
   const firstChar = title.charAt(0)
-
+  debugger;
   return (
     <Post>
       <Title>
