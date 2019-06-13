f1390853defdb707fe0f16aa7c1c662b3e438c9e
Danny Phillips <danny.phillips8@gmail.com>
2019-06-12 23:19:00 -0700

    refactor readinglist to finally look good;

--- src/components/About/ReadingList.js
+++ src/components/About/ReadingList.js
@@ -1,27 +1,22 @@
 import React from "react";
+import { Card } from "@material-ui/core";
+import Helmet from "react-helmet";
+import { Flex } from "../../components"
 
-class ReadingList extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+import "./ReadingList.css";
 
-  componentDidMount() {
-    const script = document.createElement("script");
-    script.src =
-      "https://www.goodreads.com/review/custom_widget/74627569.Danny%20Phillips's%20bookshelf:%20to-read?cover_position=left&cover_size=medium&num_books=10&order=d&shelf=to-read&show_author=1&show_cover=1&show_rating=1&show_review=1&show_tags=1&show_title=1&sort=date_added&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=1558731336&widget_text_color=000000&widget_title_size=medium&widget_width=medium";
-    script.async = true;
-
-    this.instance.appendChild(script);
-  }
-
-  render() {
-    return (
-      <div>
-        <div ref={el => (this.instance = el)} />
-        Insert Books here
-      </div>
-    );
-  }
-}
+const ReadingList = () => (
+  <Flex className="container">
+    <h3>My Bookshelf</h3>
+    <Helmet>
+      <script
+        src="https://www.goodreads.com/review/custom_widget/74627569.Danny%20Phillips's%20bookshelf:%20to-read?cover_position=left&cover_size=medium&num_books=10&order=d&shelf=to-read&show_author=1&show_cover=1&show_rating=1&show_review=1&show_tags=1&show_title=1&sort=date_added&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=1558731336&widget_text_color=000000&widget_title_size=medium&widget_width=medium"
+        type="text/javascript"
+        charset="utf-8"
+      />
+    </Helmet>
+    <Card className="list" id="gr_custom_widget_1560404559" />
+  </Flex>
+);
 
 export default ReadingList;
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import styled from 'styled-components'
-// import Helmet from "react-helmet";
+import React from "react";
+import styled from "styled-components";
 
-import { Layout, Cover, SocialLinks } from '../components'
-// import { ReadingList } from '../components'
+import { Layout, Cover, SocialLinks, ReadingList } from "../components";
 
 const Container = styled.div`
   padding: 40px;
@@ -12,21 +10,13 @@ const Container = styled.div`
 `;
 const AboutPage = () => (
   <Layout>
-    <Cover>About Me</Cover>
-    {/* <Helmet>
-      <script
-        src="https://www.goodreads.com/review/custom_widget/74627569.Danny%20Phillips's%20bookshelf:%20to-read?cover_position=left&cover_size=medium&num_books=10&order=d&shelf=to-read&show_author=1&show_cover=1&show_rating=1&show_review=1&show_tags=1&show_title=1&sort=date_added&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=1558731336&widget_text_color=000000&widget_title_size=medium&widget_width=medium"
-        type="text/javascript"
-        charset="utf-8"
-      />
-    </Helmet>
-    <ReadingList /> */}
+    {/* <Cover>About Me</Cover> */}
     <Container>My Bio</Container>
     <Container>My Skills</Container>
-    <Container>Insert Reading List</Container>
-    <Container><SocialLinks/></Container>
+    <ReadingList />
+    <SocialLinks />
     <Container>Link to Resume</Container>
   </Layout>
 );
 
-export default AboutPage
+export default AboutPage;
