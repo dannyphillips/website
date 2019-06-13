1775aaedf0bb0102df40afc4d6703c7c3f78016d
Danny Phillips <danny.phillips8@gmail.com>
2019-06-13 00:19:45 -0700

    add kebabCase function

---
+++ src/utils/kebabCase.js
@@ -0,0 +1,6 @@
+const KEBAB_REGEX = /[A-Z\u00C0-\u00D6\u00D8-\u00DE]/g;
+export default function kebabCase(str){
+  return str.replace(KEBAB_REGEX, (match) => (
+    "-" + match.toLowerCase()
+  ))
+};
